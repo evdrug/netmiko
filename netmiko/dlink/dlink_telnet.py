72e43cea174e588548a0e3ae152b756f0ecf9beb
evdrug <druzyakin.e@gmail.com>
2018-08-11 10:17:09 +0300

    fix dlink

--- netmiko/dlink/dlink_telnet.py
+++ netmiko/dlink/dlink_telnet.py
@@ -8,48 +8,41 @@ class DlinkTelnet(CiscoSSHConnection):
         self.set_base_prompt()
         self.enable()
         self.disable_paging()
-    #     self.send_command_timing("disable cli prompting")
-    #     # Clear the read buffer
+        #     self.send_command_timing("disable cli prompting")
+        #     # Clear the read buffer
         time.sleep(.3 * self.global_delay_factor)
         self.clear_buffer()
 
-    def set_base_prompt(self, pri_prompt_terminator='user#', alt_prompt_terminator='admin#', delay_factor=1):
-        delay_factor = self.select_delay_factor(delay_factor)
-        self.clear_buffer()
-        self.write_channel(self.RETURN)
-        time.sleep(.5 * delay_factor)
+    def set_base_prompt(self, pri_prompt_terminator='#',
+                        alt_prompt_terminator='#', delay_factor=1):
 
-        prompt = self.read_channel()
-        prompt = self.normalize_linefeeds(prompt)
-        # If multiple lines in the output take the last line
-        prompt = prompt.split(':')[-1]
-        prompt = prompt.strip()
-        # Check that ends with a valid terminator character
-        if not prompt in (pri_prompt_terminator, alt_prompt_terminator):
-            raise ValueError("Router prompt not found: {0}".format(prompt))
-
-
-        self.base_prompt = prompt
+        fPrompt = self.find_prompt(delay_factor=delay_factor)
+        prompt = fPrompt.split(':')
+        if not prompt[-1] in ('user#', 'admin#', '3#', '4#', '5#'):
+            raise ValueError("Router prompt not found: {0}".format(repr(prompt)))
+        # Strip off trailing terminator
+        self.base_prompt = prompt[0]
 
         return self.base_prompt
 
+    def check_enable_mode(self, check_string=''):
 
-    def enable(self, cmd='enable admin', pattern='ssWord', re_flags=re.IGNORECASE):
-        """Enter enable mode."""
-        self.base_prompt = 'admin#'
+        self.write_channel(self.RETURN)
+        output = self.read_until_prompt()
+        pattern = re.compile("(admin#|4#|5#)")
+
+        return pattern.search(output) is not None
+
+    def enable(self, cmd='enable admin', pattern=r'ss[Ww]ord', re_flags=re.IGNORECASE):
+
         return super(DlinkTelnet, self).enable(cmd=cmd, pattern=pattern, re_flags=re_flags)
-
-    def check_enable_mode(self, check_string='admin#'):
-        """Checks whether in configuration mode. Returns a boolean."""
-        return super(DlinkTelnet, self).check_config_mode(check_string=check_string)
-
-    def exit_config_mode(self, exit_config=''):
-        """No configuration mode on Extreme."""
-        return ''
 
     def disable_paging(self, command="disable clipaging", delay_factor=1):
         return super(DlinkTelnet, self).disable_paging(command=command)
 
-    def save_config(self, cmd='save', confirm=False):
-        """Saves configuration."""
-        return super(DlinkTelnet, self).save_config(cmd=cmd, confirm=confirm)
+    def telnet_login(self, pri_prompt_terminator=r'#\s*$', alt_prompt_terminator=r'>\s*$',
+                         username_pattern=r"(?:user:|username|login|user name)",
+                         pwd_pattern=r"assword",
+                         delay_factor=3, max_loops=20):
+        return super(DlinkTelnet, self).telnet_login( pri_prompt_terminator, alt_prompt_terminator,
+                         username_pattern, pwd_pattern, delay_factor, max_loops)
